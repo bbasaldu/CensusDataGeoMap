7aec57764b0859b37b263f251f039ece97f0bebe
bbasaldu <50085901+bbasaldu@users.noreply.github.com>
2020-06-11 20:41:10 -0700

    Add files via upload

--- CA.js
+++ CA.js
@@ -3,14 +3,23 @@ var height = 960;
 var mapBool = false;
 var indexGreen = 19; //2018 data for second dataset
 var indexRed = 10; //2018 data for first dataset
+//svg space for drawing big map
 var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
+//svg space for mini map
+var MiniSvg = d3.select("body")
+    .append("svg")
+    .attr("width", 400)
+    .attr("height", 320)
+    .attr("transform", "translate(510,-885)");
+
 var color = d3.scaleThreshold()
             .domain([500, 1000, 1500, 2000, 2500, 3000, 3500, 4000])
             //.domain([250, 500, 1000, 2000, 3000, 4000, 5000,6000])
             .range(d3.schemeOrRd[9]);//d3.schemeOrRd[9] //d3.schemeBlues[9]
+
 console.log(d3.schemeOrRd[9]);
 var x = d3.scaleSqrt()
     .domain([0, 5000])
@@ -74,6 +83,9 @@ function ready(Data) {
     .on('end', function(d){
         console.log(d.getFullYear());
         updateMap(d.getFullYear());
+        if(GlobalFIPS != ""){
+           outlineCounty(GlobalFIPS); 
+        }
     });
     
     var gTime = svg.append("g")
@@ -81,7 +93,7 @@ function ready(Data) {
     .attr('height', 100)
     .attr('fill', 'black')
     .append('g')
-    .attr('transform', 'translate(700,350)');
+    .attr('transform', 'translate(700,450)');
     
     console.log(sliderTime.value());
     gTime.call(sliderTime);
@@ -101,10 +113,11 @@ function ready(Data) {
 
     //console.log(topojson.feature(MapData, MapData.objects.tracs));
     //let temp;
-   
+
     //let temp;
     let DataIndex = 10; // 2018 data
     var Tracts = svg.append("g")
+        .attr("class", "tract")
         .selectAll("path")
         .data(CA.features)
         .enter().append("path")
@@ -132,7 +145,7 @@ function ready(Data) {
             .duration(200)
             .style("opacity", 0.9)
             .style("background", color(100));
-      })
+        })
         .on("mousemove", function(d){
             tooltip.html(function(b){
                 if(mapBool == false){
@@ -177,15 +190,193 @@ function ready(Data) {
             tooltip.transition()		
                 .duration(500)		
                 .style("opacity", 0);
+        })
+        .on("click", function(d){
+            //console.log(d.id.slice(0, 3));
+            outlineCounty(d.id.slice(0, 3));
         });
     
-    var Counties = svg.append("path")
-        .datum(topojson.feature(MapData, MapData.objects.counties))
+    //projection for pre projected map
+    var proj = d3.geoIdentity();
+    
+    //proj.fitExtent([[0,0], [100, 100]]);
+    var GlobalFIPS = "";
+    function outlineCounty(fips){
+        //show minimap "box"
+        d3.select("#clipRect").attr("stroke-opacity", 1);
+        GlobalFIPS = fips;
+        var CountyTracts = CA.features.filter(function(d){
+            if(d.id.slice(0, 3) == fips) return d;
+        });
+        var TractObj = {
+            "type":"FeatureCollection",
+            "features": CountyTracts
+        }
+        //console.log(CountyTracts);
+        //console.log(TractObj);
+        //console.log(CA.features);
+        //rest minimap drawing
+        reset(prevSelection);
+        //outline selected county
+        var county = d3.select("#I"+fips);
+        county.attr("stroke", "blue");
+        county.attr("stroke-opacity", 1);
+        county.attr("stroke-width", 1);
+        
+        //set new geo path for minimap data
+        var miniPath = d3.geoPath().projection(proj);
+        //drawMiniMap(county.attr("d"));
+        var geoObj = topojson.feature(MapData, MapData.objects.counties).features.find(function(d){
+            if(d.id == fips) return d;
+        });
+        //center the drawing somewhere else. to the right of big map
+        proj.fitExtent([[0,0], [400, 300]], TractObj);
+        Minimap.append("g")
+            .attr("class", "CountyTracts")
+            .selectAll("path")
+            .data(CountyTracts)
+            .enter().append("path")
+            .attr("stroke", "#000")
+            .attr("stroke-opacity", 1)
+            .attr("stroke-width", 0.1)
+            .attr("clip-path", "url(#rectClip)")
+            .attr("fill", function(d){
+              var DataIndex;
+              if(mapBool == false) DataIndex = indexRed;
+              else DataIndex = indexGreen;
+              var temp = Data[DataIndex].find(function(d2){
+                if(d2[2] == d.id.slice(0,3) && d2[3] == d.id.slice(3,9)){
+                    //console.log(+d2[0]);
+                    return d2;
+                }
+                else if(d.id.slice(3,9) == "137000"){//tract 135202 no longer exists after 2011. And becomes a new one
+                    var arr = [0,0,0];
+                    return arr;
+                }
+                //console.log(d2);
+            });
+
+            return color(+temp[0]);            
+            })
+            .attr("d", miniPath)
+            .on("mouseover", function(d){
+          tooltip.transition()
+            .duration(200)
+            .style("opacity", 0.9)
+            .style("background", color(100));
+        })
+        .on("mousemove", function(d){
+            tooltip.html(function(b){
+                if(mapBool == false){
+                var CountyObj = Data[1].find(function(c){
+                    if(c[2] == d.id.slice(0, 3)){
+                        return c;
+                    }
+                });
+                var TractObj = Data[indexRed].find(function(d2){
+                if(d2[2] == d.id.slice(0,3) && d2[3] == d.id.slice(3,9)){
+                    return d2;
+                    }
+                });
+                var temp = +TractObj[0];
+                var cost = "$"+d3.format(",")(temp);
+                //console.log(temp);
+                if(temp < 0) cost = "n/a";
+                return CountyObj[0] + "<br><br>"+"Avg. Monthly Cost: " + cost;
+                }
+                else{
+                console.log(indexGreen);
+                var CountyObj = Data[1].find(function(c){
+                    if(c[2] == d.id.slice(0, 3)){
+                        return c;
+                    }
+                });
+                var TractObj = Data[indexGreen].find(function(d2){
+                if(d2[2] == d.id.slice(0,3) && d2[3] == d.id.slice(3,9)){
+                    return d2;
+                    }
+                });
+                var temp = +TractObj[0];
+                var PovertyCount = d3.format(",")(temp);
+                //console.log(temp);
+                return CountyObj[0] + "<br><br>"+"# of Latinx below Poverty Line: " + PovertyCount;                    
+                }
+            })
+                .style("left", (event.clientX + 10) + "px") //position tooltip at mouse position
+                .style("top", (event.clientY) + "px");
+             })
+        .on("mouseout", function(d) {	//make tooltip fade away	
+            tooltip.transition()		
+                .duration(500)		
+                .style("opacity", 0);
+        });
+        
+        var CountyObj = Data[1].find(function(c){
+            if(c[2] == fips){
+                return c;
+            }
+        });
+        //console.log(CountyObj);
+        //change minimap caption
+        d3.select("#MinimapCaption")
+            .text(CountyObj[0]+" Census Tracts");
+        
+        //global var to delete last drawing
+        prevSelection = county;
+        //county.attr("stroke", color(100));
+        //console.log(county.attr("d"));
+    }
+    //get rid of last minimap drawing and update. also reset zoom/drag
+    function reset(prev){
+        if(prev != null){
+            prev.attr("stroke", "#000");
+            prev.attr("stroke-opacity", 0.5);
+            prev.attr("stroke-width", 0.5);
+            Minimap.call(zoom.transform, d3.zoomIdentity);
+        }
+        Minimap.selectAll("path").remove().exit();
+    }
+    
+    var prevSelection = null;
+    
+    var Counties = svg.append("g")
+        .attr("class", "counties")
+        .selectAll("path")
+        .data(topojson.feature(MapData, MapData.objects.counties).features)
+        .enter().append("path")
+        .attr("id", function(d){ return "I"+d.id; })
         .attr("fill", "none")
         .attr("stroke", "#000")
-        .attr("stroke-opacity", 1)
-        .attr("d", path);
+        .attr("stroke-opacity", 0.5)
+        .attr("stroke-width", 0.5)
+        .attr("d", path)
+        .on("click", function(d){
+            console.log(d.id);
+            //reset();
+        });
     
+    //Add minimap group
+    var Minimap = MiniSvg.append("g")
+        .attr("class", "minimap")
+        //.attr("transform", "translate(500,100)");
+    //add Minimap "box"
+    Minimap.append("rect")
+        .attr("id", "clipRect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 400)
+        .attr("height", 320)
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .attr("stroke-opacity", 0);
+    
+    //holder for minimap text
+    svg.append("text")
+        .attr("id", "MinimapCaption")
+        .attr("x", 700)
+        .attr("y", 440)
+        .text("");
+        
     var GreenBtn = svg.append("rect")
         .attr("x", width-100)
         .attr("y", height/8 + 150)
@@ -194,6 +385,7 @@ function ready(Data) {
         .attr("stroke", "black")
         .attr("stroke-opacity", 0)
         .attr("fill", d3.schemeGreens[9][3]);
+    
     svg.append("text")
         .attr("x", width-90)
         .attr("y", height/8 + 180)
@@ -220,6 +412,7 @@ function ready(Data) {
         color.domain([100, 250, 500, 1000,2000, 3000, 4000, 5000])
         x.domain([0, 6000])
         console.log(indexGreen);
+        var fips = "";
         Tracts.transition()
             .duration(3000)
             .ease(d3.easeQuad)
@@ -240,6 +433,8 @@ function ready(Data) {
         });
         mapBool = true;
         updateLegend();
+        outlineCounty(GlobalFIPS);
+        
         }
     });
     
@@ -270,6 +465,7 @@ function ready(Data) {
         });
         mapBool = false;
         updateLegend();
+        outlineCounty(GlobalFIPS);
         }
     });
     
@@ -382,9 +578,12 @@ function updateLegend(){
   .select(".domain")
     .remove();
  };  
+    Minimap.call(zoom);
     //svg.call(zoom) //zoom/drag functionality but it gets laggy.
     function zoomed (){
-        svg.selectAll("path")
-            .attr("transform", d3.event.transform);
+        tooltip.style("left", (event.clientX + 10) + "px") //position tooltip at mouse position
+                .style("top", (event.clientY) + "px");
+        Minimap.selectAll("path")
+            .attr("transform", d3.event.transform); 
     }
 }
